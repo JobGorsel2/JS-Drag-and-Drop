2a236d8a7122ef919a33a6b8ab1e602dc0eeb77f
Job <jobvangorsel@hotmail.nl>
2022-11-17 15:55:00 +0100

    Commit 2

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
-var data = [
-    {
-        id:1,
-        value:'Item1',
-    },
-    {
-        id:2,
-        value:'Item2',
-    },
-    {
-        id:3,
-        value:'Item3',
-    }
-]
+// var data = [
+//     {
+//         id:1,
+//         value:'Item1',
+//     },
+//     {
+//         id:2,
+//         value:'Item2',
+//     },
+//     {
+//         id:3,
+//         value:'Item3',
+//     }
+// ]
 
-let dragged;
+// let dragged;
 
-/* events fired on the draggable target */
-const source = document.getElementById("draggable");
-source.addEventListener("drag", (event) => {
-  console.log("dragging");
-});
+// /* events fired on the draggable target */
+// const source = document.getElementById("draggable");
+// source.addEventListener("drag", (event) => {
+//   console.log("dragging");
+// });
 
 
-const traget = document.getElementById("droptarget");
-traget.addEventListener("dragover", (event) => {
+// const traget = document.getElementById("droptarget");
+// traget.addEventListener("dragover", (event) => {
 
-    event.preventDefault();
+//     event.preventDefault();
 
-}, false);
+// }, false);
